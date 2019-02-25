24e03e2fda1bc614e57c442d188f693de33d7f0c
jorgenmiller <jorgen.j.miller@gmail.com>
2019-02-25 17:38:58 -0500

    led test programs

--- led_blink.py
+++ led_blink.py
@@ -1,15 +1,21 @@
-#Raspberry Pi GPIO led test
-#turns on an led
+#Raspberry Pi GPIO led blink
+#flashes led for 1 second
 #B17 -> led -> 200 ohm resistor -> ground
 
 import RPi.GPIO as GPIO
+import time
 
 def setup():
     GPIO.setmode(GPIO.BCM) #pins identified by BCM numbering system
     GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) #led starts as off
 
 def main():
-    GPIO.output(17, GPIO.HIGH) #led on
+    while True:
+        GPIO.output(17, GPIO.HIGH) #led on
+        time.sleep(1)
+
+        GPIO.output(17, GPIO.LOW) #led off
+        time.sleep(1)
 
 try:
     setup()
